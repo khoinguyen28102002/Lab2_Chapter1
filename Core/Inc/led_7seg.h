bc3987628924c3159098c661f3a1058aab0da9d5
khoinguyen28102002 <khoinguyennguyen914@gmail.com>
2024-11-11 11:49:23 +0700

    Chapter 2

---
+++ Core/Inc/led_7seg.h
@@ -0,0 +1,21 @@
+/*
+ * led_7seg.h
+ *
+ *  Created on: Nov 11, 2024
+ *      Author: NGUYEN KHOI NGUYEN
+ */
+
+#ifndef INC_LED7SEG_H_
+#define INC_LED7SEG_H_
+
+#include "spi.h"
+
+void led7_init();
+void led7_Scan();
+void led7_SetDigit(int num, int position, uint8_t show_dot);
+void led7_SetColon(uint8_t status);
+
+void led_On(uint8_t index);
+void led_Off(uint8_t index);
+
+#endif /* INC_LED7SEG_H_ */
